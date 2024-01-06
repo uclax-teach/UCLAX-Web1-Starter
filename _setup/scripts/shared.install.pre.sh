0b2596e9788c638b5482456b992ded7172ed8d4e
Mitch Gohman <meatch@me.com>
2024-01-06 16:23:54 -0700

    Setup: cleanup changes

--- _setup/scripts/shared.install.pre.sh
+++ _setup/scripts/shared.install.pre.sh
@@ -60,8 +60,9 @@ echo "$globalScriptTitle Git: Update Author Name and Email"
 git config --global user.name "$ufname $ulname"
 git config --global user.email "$uemail"
 
+echo "$globalScriptTitle Git: Set git default branch back to the original \"master\" branch. In case they tried to change it to main; which is silly."
+git config --global init.defaultbranch "master"
+
 echo "$globalScriptTitle Git: Use VS Code as Git Editor"
 git config --global core.editor "code --wait"
 
-echo "$globalScriptTitle Git: Set git default branch back to the original \"master\" branch. In case they tried to change it to main; which is silly."
-git config --global init.defaultbranch "master"
