9a04059b6c0074cf749c5e96961ae1e0c8456b14
Mitch Gohman <meatch@me.com>
2024-01-07 09:28:45 -0700

    SETUP: WIN: Remove NVM commands in favor of manual installation in instructions

--- _setup/scripts/win.nvm.install.sh
+++ _setup/scripts/win.nvm.install.sh
@@ -3,6 +3,3 @@
 # Install NVM, Node and NPM
 echo "$globalScriptTitle $childScriptTitle Install NVM, Node and NPM"
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-nvm install v20.9.0
-nvm use v20.9.0
-nvm alias default v20.9.0
